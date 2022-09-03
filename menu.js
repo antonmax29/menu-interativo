e5a1c61d7c8afbb088d4340924d8abeb80c31616
antonmax29 <antssilva1992@gmail.com>
2022-09-02 21:06:12 -0300

    menu finalizado

---
+++ menu.js
@@ -0,0 +1,29 @@
+const setaNext = document.querySelector('.btn-seta');
+const setaPrevious = document.querySelector('.btn-seta2');
+const container = document.querySelector('.container');
+
+const texto = document.querySelectorAll('.texto');
+setaNext.addEventListener('click', aumentaTamanho);
+setaPrevious.addEventListener('click', diminuiTamanho);
+
+function aumentaTamanho() {
+    container.style.width = "300px";
+    document.querySelector('.item').style.width = "270px";
+    setaNext.style.display = "none"
+    setaPrevious.style.display = "block";
+texto.forEach(texto => {
+    texto.style.display = "block"
+    });
+   
+};
+
+function diminuiTamanho(){
+    document.querySelector('.container').style.width = "60px";
+    document.querySelector('.item').style.width = "";
+    setaPrevious.style.display = "none";
+    setaNext.style.display = 'block';
+    texto.forEach(texto => {
+        texto.style.display = "none"
+        });
+    
+};
